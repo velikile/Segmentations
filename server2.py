7213c74b94a88755df0870ca0789001999ed3fa4
Ubuntu <ubuntu@ip-172-31-6-47.ec2.internal>
2021-06-20 14:24:43 +0000

    Add colorization to segmentation result

--- server2.py
+++ server2.py
@@ -14,21 +14,33 @@ import numpy as np
 import torch
 from PIL import Image
 from torchvision import transforms
+import random as rnd
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 model = torch.load("mrcnn.m")
 model.eval()
-
+rand_colors = np.random.randint(0,255,size=(1000,3))
 def ProduceMask(fileBytes): 
     img = transforms.ToTensor()(Image.open(BytesIO(fileBytes)))
     with torch.no_grad():
         prediction = model([img.to(device)])
-        imgmask = Image.fromarray(prediction[0]['masks'][0, 0].mul(255).byte().cpu().numpy())
+        imgmask   = Image.fromarray(prediction[0]['masks'][0, 0].mul(255).byte().cpu().numpy())
+        maskcolor = np.stack((imgmask,)*3,axis=-1)
         for m in range(prediction[0]['masks'].shape[0]):
-            npmask = np.asarray(imgmask) | prediction[0]['masks'][m, 0].mul(255).byte().cpu().numpy()
-            imgmask = Image.fromarray(npmask)
-           # imgmask = Image.blend(imgmask, Image.fromarray(prediction[0]['masks'][m, 0].mul(255).byte().cpu().numpy()),0.5)
+            pred = prediction[0]['masks'][m, 0].mul(255).byte().cpu().numpy()
+            pred = np.stack((pred,)*3,axis=-1)
+            r_color = rand_colors[m%100] 
+            r = pred[:,:,0] 
+            g = pred[:,:,1] 
+            b = pred[:,:,2] 
+            r[r!=0] = r_color[0]
+            g[g!=0] = r_color[1]
+            b[b!=0] = r_color[2]
+            
+            maskcolor = maskcolor| pred  
+            imgmask = Image.fromarray(maskcolor)
+
         tmp = BytesIO()
         imgmask.save(tmp,format="png")
         tmp.seek(0)
